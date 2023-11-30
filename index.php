cab3c37e391176a5280c0337ed317722658abb8d
Erst086 <eresto08@gmail.com>
2023-11-30 17:26:20 -0600

    se paso la pagina sola a php se hacen pruebas

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="icon" type="image/jpg" href="recursos/favicon_perfil.jpg" />
+    <meta name="description" content="Pagina de inicio Registro y Logueo">
+    <meta name="keywords" content="HTML, Basico, Curso, UNAM, ICO">
+    <meta name="author" content="Ernesto Contreras">
+    /<!--//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+        rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@1,500&display=swap"
+        rel="stylesheet">
+
+    <link rel="stylesheet" href="css/style.css">
+
+
+    <title>GA_Proyects</title>
+</head>
+<header>
+    <h1>GA Proyects </h1>
+    <section id="logo">
+        <img src="recursos/logo.png" alt="logoEmpresa" width="150px" height="150px">
+    </section>
+</header>
+
+<body>
+    <main>
+
+        <head></head>
+        <div class="Recuadro_LRG">
+            <div class="box_trasera">
+                <div class="box_trsr_login">
+                    <h3>¿Ya tienes una cuenta?</h3>
+                    <p>Inicia sesion para entrar en la pagina</p>
+                    <button id="btn_iniciar-sesion">Iniciar Sesión</button>
+                </div>
+                <div class="box_trsr_registro">
+                    <h3>¿Aun no tienes una cuenta?</h3>
+                    <p>Registrate para que puedas iniciar sesion</p>
+                    <button id="btn_registrarse">Registrarse</button>
+                </div>
+            </div>
+
+            <!--Formulario de Login y registro-->
+            <div class="login-register">
+                <!--Login-->
+                <form action="" class="FML-login">
+                    <h2>Iniciar Sesion</h2>
+                    <input type="text" placeholder="Correo Electronico">
+                    <input type="password" placeholder="Contraseña">
+                    <button>Entrar</button>
+                </form>
+
+                <!--Registro-->
+                <form action="" class="FML-register">
+                    <h2>Registrarse</h2>
+                    <input type="text" placeholder="Nombre completo">
+                    <input type="text" placeholder="Nombre de Usuario">
+                    <input type="text" placeholder="Correo Electronico">
+                    <input type="text" placeholder="Usuario">
+                    <input type="password" placeholder="Contraseña">
+                    <button>Registrarse</button>
+                    <h6><a href="politica.html" style="color: black;">Precione para leer las políticas de privacidad ... je je</a></h6>
+                </form>
+            </div>
+        </div>
+    </main>
+    <script src="js/scrip.js"></script>
+</body>
+
+</html>
